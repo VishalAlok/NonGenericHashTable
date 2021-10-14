162ce0756a1ca24f268f4e6d8464a0fd212a84fb
vishal.Alok <you@example.com>
2021-10-14 09:50:00 +0530

    Add project files.

---
+++ NonGenericHashTable/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericHashTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add("Ename", "Scott");
+            ht.Add("Salary", 25000.00);
+            ht.Add("Phone", "9999999999");
+            ht.Add("Email", "scott@gmail.com");
+            Console.WriteLine(ht["Ename"].GetHashCode());
+   
+            foreach (object key in ht.Keys)
+            {
+                Console.Write(key + " : "+ ht[key]); //Will Give only value
+                Console.WriteLine();
+            }
+        }
+    }
+}
